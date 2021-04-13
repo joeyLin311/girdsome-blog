1da6acd6150887b32ee7fb4d14ba3823615bdb30
linJoey <qiaonanlin@gmail.com>
2021-04-13 23:07:38 +0800

    format code

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -1,23 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-13 21:51:23
+ * @LastEditTime: 2021-04-13 22:24:27
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \girdsome-blog\src\components\Latest.vue
+-->
 <template>
-    <div>
-        <div class="latest-journals-heading container">
-            <span class="label">Latest and greatest</span>
-        </div>
-        <div class="latest-journals">
-            <div class="container">
-                <a href="/journal/gridsome-forestry-cms/" class="journal" v-for="latest in $page.latests.edges" :key="latest.node.title">
-                    <h3 class="journal-title">
-                        {{latest.node.title}}
-                    </h3>
-                </a>
-            </div>
-        </div>
+  <div>
+    <div class="latest-journals-heading container">
+      <span class="label">Latest and greatest</span>
     </div>
+    <div class="latest-journals">
+      <div class="container">
+        <a
+          href="/journal/gridsome-forestry-cms/"
+          class="journal"
+          v-for="latest in $page.latests.edges"
+          :key="latest.node.title"
+        >
+          <h3 class="journal-title">
+            {{ latest.node.title }}
+          </h3>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "latest",
+  name: "latest",
 };
 </script>
 
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,28 +1,40 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-10 17:44:19
+ * @LastEditTime: 2021-04-13 22:24:14
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \girdsome-blog\src\layouts\Default.vue
+-->
 <template>
-    <div class="layout">
-        <header class="header sticky">
-            <div class="container">
-                <div class="left">
-                    <a href="/" class="home-link active--exact active">
-                      <img src="/img/logo.332701b7.svg" alt="Nichlas W. Andersen" class="logo"/>
-                    </a>
-                </div>
-                <nav class="nav right">
-                    <a href="/journal" class="nav__link">Journal</a>
-                    <a href="/contact" class="nav__link">Say Hi!</a>
-                </nav>
-            </div>
-        </header>
+  <div class="layout">
+    <header class="header sticky">
+      <div class="container">
+        <div class="left">
+          <a href="/" class="home-link active--exact active">
+            <img
+              src="/img/logo.332701b7.svg"
+              alt="Nichlas W. Andersen"
+              class="logo"
+            />
+          </a>
+        </div>
+        <nav class="nav right">
+          <a href="/journal" class="nav__link">Journal</a>
+          <a href="/contact" class="nav__link">Say Hi!</a>
+        </nav>
+      </div>
+    </header>
 
-        <!-- 子组件出口 -->
-        <slot></slot>
+    <!-- 子组件出口 -->
+    <slot></slot>
 
-        <footer class="footer">
-            <div class="container">
-                <span>Copyright © 2021</span>
-            </div>
-        </footer>
-    </div>
+    <footer class="footer">
+      <div class="container">
+        <span>Copyright © 2021</span>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <static-query>
@@ -34,5 +46,4 @@ query {
 </static-query>
 
 <style>
-
 </style>
